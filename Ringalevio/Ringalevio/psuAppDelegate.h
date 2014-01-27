529ceee9888b31e63b27bc690beb880e3152555a
Kavon Farvardin <kavon.farvardin@gmail.com>
2014-01-26 22:11:03 -0500

    Forgot to add sources.

---
+++ Ringalevio/Ringalevio/psuAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  psuAppDelegate.h
+//  Ringalevio
+//
+//  Created by Kavon Farvardin on 1/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface psuAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
