7ea787b90bae6a915f7c0970fa9d2f98e15ce096
Kavon Farvardin <kavon.farvardin@gmail.com>
2014-01-26 22:12:03 -0500

    More files

---
+++ Ringalevio/Ringalevio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ringalevio
+//
+//  Created by Kavon Farvardin on 1/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "psuAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([psuAppDelegate class]));
+    }
+}
