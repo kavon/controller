0a92d8dc1a71159268e5bfc282dae6ae81a5c661
Tim Sexton <timmys@comcast.net>
2014-03-25 06:56:40 -0400

    Online Map view defaults to State College PA

--- Ringalevio/Ringalevio/OnlineMapViewController.m
+++ Ringalevio/Ringalevio/OnlineMapViewController.m
@@ -30,11 +30,21 @@
     {
         [super viewDidLoad];
         
-        RMMapboxSource *tileSource = [[RMMapboxSource alloc] initWithMapID:@"djtsex.heamjmoi"];
+        RMMapboxSource *tileSource = [[RMMapboxSource alloc] initWithMapID:@"djtsex.heamjmoi" ];
         
         RMMapView *mapView = [[RMMapView alloc] initWithFrame:self.view.bounds andTilesource:tileSource];
         
         [self.view addSubview:mapView];
+        
+        // Default View to State College
+        CLLocationCoordinate2D NorthEastStateCollege = CLLocationCoordinate2DMake(40.8196546,-77.8336887);
+        
+        CLLocationCoordinate2D SouthWestStateCollege = CLLocationCoordinate2DMake(40.7811256,-77.8711967);
+        
+        CLLocationCoordinate2D StateCollege = CLLocationCoordinate2DMake(40.7880144,-77.8525715);
+        
+        [mapView setZoom:13 atCoordinate:StateCollege animated:YES];
+        
     }
 
 
