208d1f803def4b4f512974bdd6ca64b33715b971
sean-lane <sjl5306@psu.edu>
2014-04-30 19:12:07 -0400

    Initial gyro support for sensor view.

--- Ringalevio/Ringalevio/SensorViewController.h
+++ Ringalevio/Ringalevio/SensorViewController.h
@@ -6,12 +6,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 #import "MissionItem.h"
 
 @interface SensorViewController : UIViewController
 
 // Mission item for recieving URL
 @property MissionItem* mi;
+
+//variables for gyro
+@property NSString *x;
+@property NSString *y;
+@property NSString *z;
+
+// controller for gyro
+@property CMMotionManager* motionManager;
 
 - (void)reloadStream;
 
